15e337cd86a33e042d3e71b80d9399e879ad9736
RadioGenX <RadioGenX@radiogenx.com>
2024-09-24 23:52:42 -0400

    copy other themes from premium source folder

---
+++ themes/white/plugins/default/css/search.php
@@ -0,0 +1,34 @@
+.ossn-search {
+   display:inline-block;
+   width:60%;
+   margin:11px;
+}
+.ossn-search input[type='text']{
+    width: 100%;
+    margin: 0 auto;
+    color: var(--text-color);
+    border-radius: 30px;
+    background: var(--bg-input-bg);
+    border: 0;
+    padding: 9px 9px 9px 30px;
+}
+.ossn-search:before { 
+	font-family: 'Font Awesome 5 Free';
+    content: "\f002";
+    font-weight: bold;
+    color: var(--text-color);
+    position: absolute;
+    margin: 10px;
+}
+.ossn-search input[type='text']:focus::placeholder {
+  color: transparent;
+}
+.ossn-search:focus-within {
+	width:65%;
+}
+.search-topbar-sm .ossn-search {
+	width:70%;
+}
+.search-topbar-sm .ossn-search:focus-within {
+	width:90%;
+}
